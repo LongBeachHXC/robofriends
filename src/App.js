1bd9cb9d1af9a0a68e8b6a810aa3ecdfa069900d
LongBeachHXC <fweinberg@live.com>
2018-02-23 22:15:05 -0800

    Changes implemented after Building React App 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import CardList from './cardlist';
+import SearchBox from './searchbox';
 import { robots } from './robots';
 
-const App = () => {
-    return (
-        <CardList robots={robots} />
-    );
+const state = {
+    robots: robots,
+    searchfield: ''
+}
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots:robots,
+            searchfield:''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+    }
+
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()
+        })
+        return (
+            <div className= 'tc'>
+                <h1>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
+            </div>
+        )
+    };
 
 }
 
