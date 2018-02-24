81b252fcaba83706818c2cc0c0ffd52ba2c65ad1
LongBeachHXC <fweinberg@live.com>
2018-02-23 20:53:04 -0800

    Minor Change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './card';
+// import Card from './card';
 import CardList from './cardlist'
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
