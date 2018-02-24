f3bc7e68aaed22122a0bbbcb654e5c5dc3016fe7
LongBeachHXC <fweinberg@live.com>
2018-02-23 21:28:21 -0800

    Changes implemented after Building React App 3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import Card from './card';
 import App from './App'
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
 
-ReactDOM.render(document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
