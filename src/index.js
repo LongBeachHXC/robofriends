5c4844bb81e907586db0cda4140ca2858b3a5ebb
LongBeachHXC <fweinberg@live.com>
2018-02-23 20:25:26 -0800

    Syntax update to index.js file

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ ReactDOM.render(
                     <Card id={robots[0].id} name={robots[0].name} email= {robots[0].email} />
                     <Card id={robots[1].id} name={robots[1].name} email= {robots[1].email} />
                     <Card id={robots[2].id} name={robots[2].name} email= {robots[2].email} />
-                <div>,
+                </div>,
                 document.getElementById('root'));
 registerServiceWorker();
