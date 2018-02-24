4d100ad9cdfc6c851b51435d9750327593e54b39
LongBeachHXC <fweinberg@live.com>
2018-02-24 15:54:46 -0800

    Config changes made after the react videos

--- src/containers/App.js
+++ src/containers/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response=> response.json())
-        .then(users => this.setState({ robots: users}));
+        .then(users => {this.setState({ robots: users})});
     }
 
     onSearchChange = (event) => {
@@ -30,17 +30,18 @@ class App extends Component {
             return robots.name.toLowerCase().includes(searchfield.toLowerCase())
         })
         return !robots.length ?
-            <h1>Loading</h1>
-            (
-                <div className= 'tc'>
-                    <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
-                        <CardList robots={filteredRobots} />
-                    </Scroll>
-                </div>
-            )
-        };
+            <h1>Loading</h1> :
+        (
+            <div className= 'tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>
+            </div>
+        );
+    }
+}
 
 
 export default App;
